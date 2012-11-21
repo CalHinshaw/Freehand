00764fd8c9c5e67f89dcfeecf0d5eb01d233a2c9
Cal Hinshaw <nospam@nospam.com>
2012-11-21 09:43:51 -0800

    implemented basic NoteExplorer

--- src/com/calhounhinshaw/freehandalpha/MainMenu.java
+++ src/com/calhounhinshaw/freehandalpha/MainMenu.java
@@ -1,20 +1,36 @@
 package com.calhounhinshaw.freehandalpha;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainMenu extends Activity {
+	
+	NoteExplorer mExplorer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        
+        mExplorer = (NoteExplorer) findViewById(R.id.noteExplorer);
+        mExplorer.setRootDirectory(Environment.getExternalStoragePublicDirectory("Freehand"));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
+    }
+    
+    @Override
+    public void onBackPressed() {
+    	if (mExplorer.isInRootDirectory()) {
+    		super.onBackPressed();
+    	} else {
+    		mExplorer.moveUpDirectory();
+    	}
     }
 }
