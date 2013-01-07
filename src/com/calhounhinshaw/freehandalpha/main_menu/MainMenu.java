4860f46201240594dc2022af341c224cc4ce9b15
Cal Hinshaw <calhinshaw@gmail.com>
2013-01-07 00:16:01 -0800

    lots of MainMenu boilerplate. lots.

--- src/com/calhounhinshaw/freehandalpha/main_menu/MainMenu.java
+++ src/com/calhounhinshaw/freehandalpha/main_menu/MainMenu.java
@@ -12,23 +12,103 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnDragListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainMenu extends Activity implements IActionBarListener {
 	
-	NoteExplorer mExplorer;
-	LinearLayout itemsSelectedActionBar;
-	LinearLayout defaultActionBar;
-
+	private NoteExplorer mExplorer;
+	
+	// itemsSelectedActionBar view references
+	private LinearLayout itemsSelectedActionBar;
+	private Button selectedCancelButton;
+	private Button selectedDeleteButton;
+	
+	private OnClickListener cancelButtonOnClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TODO implement cancleButtonOnClickListener	
+		}
+	};
+	
+	private OnClickListener deleteButtonOnClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TODO implement deleteButtonOnClickListener	
+		}
+	};
+	
+	private OnDragListener cancelButtonDragListener = new OnDragListener () {
+		public boolean onDrag(View v, DragEvent event) {
+			// TODO implement cancelButtonDragListener
+			return true;
+		}
+	};
+	
+	private OnDragListener deleteButtonDragListener = new OnDragListener () {
+		public boolean onDrag(View v, DragEvent event) {
+			// TODO implement deleteButtonDragListener
+			return true;
+		}
+	};
+	
+	// defaultActionBar view references
+	private LinearLayout defaultActionBar;
+	private Button defaultNewNoteButton;
+	private Button defaultNewFolderButton;
+	private Button defaultShowAllNotesButton;
+	
+	private OnClickListener newNoteButtonOnClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TODO implement newNoteButtonOnClickListener
+		}
+	};
+	
+	private OnClickListener newFolderButtonOnClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TODO implement newFolderButtonOnClickListener
+		}
+	};
+	
+	private OnClickListener showAllNotesButtonOnClickListener = new OnClickListener() {
+		public void onClick(View v) {
+			// TODO implement showAllNotesButtonOnClickListener
+		}
+	};
+	
+	
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         
+        // Set up the defaultActionBar
         defaultActionBar = (LinearLayout) findViewById(R.id.defaultActionBar);
+        
+        defaultNewNoteButton = (Button) findViewById(R.id.newNoteButton);
+        defaultNewNoteButton.setOnClickListener(newNoteButtonOnClickListener);
+        
+        defaultNewFolderButton = (Button) findViewById(R.id.newFolderButton);
+        defaultNewFolderButton.setOnClickListener(newFolderButtonOnClickListener);
+        
+        defaultShowAllNotesButton = (Button) findViewById(R.id.showAllNotesButton);
+        defaultShowAllNotesButton.setOnClickListener(showAllNotesButtonOnClickListener);
+        
+        // Set up the itemsSelectedActionBar
         itemsSelectedActionBar = (LinearLayout) findViewById(R.id.itemsSelectedActionBar);
+        
+        selectedCancelButton = (Button) findViewById(R.id.cancelButton);
+        selectedCancelButton.setOnClickListener(cancelButtonOnClickListener);
+        selectedCancelButton.setOnDragListener(cancelButtonDragListener);
+        
+        selectedDeleteButton = (Button) findViewById(R.id.deleteButton);
+        selectedDeleteButton.setOnClickListener(deleteButtonOnClickListener);
+        selectedDeleteButton.setOnDragListener(deleteButtonDragListener);
         
         itemsSelectedActionBar.setVisibility(View.INVISIBLE);
         
