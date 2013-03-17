420d1697109238d5e5e8f5a63fe0940f3a21bbbe
Cal Hinshaw <calhinshaw@gmail.com>
2013-03-17 03:49:38 -0700

    Overrode Point's equals and toString methods.

--- src/com/calhounhinshaw/freehandalpha/note_editor/Point.java
+++ src/com/calhounhinshaw/freehandalpha/note_editor/Point.java
@@ -8,4 +8,20 @@ public final class Point {
 		this.x = x;
 		this.y = y;
 	}
+	
+	@Override
+	public boolean equals (Object o) {
+		Point p = (Point) o;
+		
+		if (Math.abs(p.x - x) <= 0.0001 && Math.abs(p.y - y) <= 0.0001) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString () {
+		return ("(" + Float.toString(x) + ", " + Float.toString(y) + ")");
+	}
 }
