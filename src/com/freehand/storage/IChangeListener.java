bfeb93be059e0af3636a0ae620453774e85fbf47
Cal Hinshaw <calhinshaw@gmail.com>
2013-08-20 02:25:20 -0700

    deleted old files no longer in use

--- src/com/freehand/storage/IChangeListener.java
+++
@@ -1,5 +0,0 @@
-package com.freehand.storage;
-
-public interface IChangeListener {
-	public void onChange();
-}
--- src/com/freehand/storage/INoteHierarchyItemSorter.java
+++
@@ -1,8 +0,0 @@
-package com.freehand.storage;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface INoteHierarchyItemSorter {
-	public ArrayList<INoteHierarchyItem> sort (List<INoteHierarchyItem> toSort);
-}
