0dabba48caa18b2eb33594783113f3368d0c49aa
Cal Hinshaw <calhinshaw@gmail.com>
2013-08-31 23:06:00 -0700

    seperated layouts into vertical phone and everything else

--- src/com/freehand/tutorial/TutorialActivity.java
+++ src/com/freehand/tutorial/TutorialActivity.java
@@ -7,14 +7,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.Gallery;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.RelativeLayout.LayoutParams;
 
 public class TutorialActivity extends Activity {
+	
+	private ImageSwitcher mSwitcher;
+	private final Integer[] slides = { 	R.drawable.erase_button, R.drawable.folder };
+	private int currentSlideIndex = 0;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,44 +23,49 @@ public class TutorialActivity extends Activity {
         this.setTitle("Freehand Tutorial");
         setContentView(R.layout.tutorial_layout);
         
-        Gallery gallery = (Gallery) this.findViewById(R.id.tutorial_gallery);
-        gallery.setAdapter(new ImageAdapter());
-
         Button skipButton = (Button) this.findViewById(R.id.skip_tutorial_button);
         skipButton.setOnClickListener(new OnClickListener() {
-
 			public void onClick(View v) {
 				Intent startOrganizer = new Intent(getBaseContext(), MainMenuActivity.class);
 				startActivity(startOrganizer);
 			}
-        	
         });
+        
+        Button prevButton = (Button) this.findViewById(R.id.tutorial_previous_button);
+        prevButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				updateSlide(-1);
+			}
+        });
+        
+        Button nextButton = (Button) this.findViewById(R.id.tutorial_next_button);
+        nextButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				updateSlide(1);
+			}
+        });
+        
+        mSwitcher = (ImageSwitcher) this.findViewById(R.id.tutorial_slideshow);
+        updateSlide(0);
 	}
 	
-	private class ImageAdapter extends BaseAdapter {
-		private final Integer[] slides = { 	R.drawable.erase_button,
-											R.drawable.folder };
-		
-		public int getCount() {
-			return slides.length;
-		}
-
-		public Object getItem(int position) {
-			return slides[position];
-		}
-
-		public long getItemId(int position) {
-			return 0;
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ImageView i = new ImageView(TutorialActivity.this);
-			i.setImageResource(slides[position]);
-			i.setLayoutParams(new Gallery.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-			i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-			return i;
+	private void updateSlide (int delta) {
+		currentSlideIndex += delta;
+		if (currentSlideIndex < 0) {
+			currentSlideIndex = 0;
+		} else if (currentSlideIndex >= slides.length) {
+			currentSlideIndex = slides.length-1;
 		}
 		
+        ImageView newSlide = new ImageView(this);
+        newSlide.setImageResource(slides[currentSlideIndex]);
+        
+        if (mSwitcher.getChildCount() > 0) {
+        	mSwitcher.removeViewAt(0);
+        }
+        
+        mSwitcher.addView(newSlide);
+        mSwitcher.showNext();
+        
 	}
-	
 }
