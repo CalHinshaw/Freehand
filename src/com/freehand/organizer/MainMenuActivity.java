00f5f97cd5f5da5762d7758f8991e5d487434708
Cal Hinshaw <calhinshaw@gmail.com>
2014-04-18 01:09:22 -0700

    Untested IAP signin and pro lisence check added

--- src/com/freehand/organizer/MainMenuActivity.java
+++ src/com/freehand/organizer/MainMenuActivity.java
@@ -2,6 +2,10 @@ package com.freehand.organizer;
 
 import java.io.File;
 
+import com.android.vending.billing.IabHelper;
+import com.android.vending.billing.IabResult;
+import com.android.vending.billing.Inventory;
+import com.android.vending.billing.Purchase;
 import com.calhounroberthinshaw.freehand.R;
 
 import com.freehand.editor.canvas.Note;
@@ -18,6 +22,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.Menu;
 import android.view.View;
@@ -30,6 +35,10 @@ import android.widget.Button;
 public class MainMenuActivity extends Activity {
 	private static final long VIBRATE_DURATION = 50;
 	private static boolean isRunning = false;
+	
+	private IabHelper iabHelper;
+	private static final String SKU_PRO = "pro";
+	private boolean hasProLisence = false;
 	
 	private FolderBrowser mBrowser;
 	
@@ -283,6 +292,8 @@ public class MainMenuActivity extends Activity {
         mBrowser.setRootDirectory(rootDirectory);
         mBrowser.setMainMenuActivity(this);
         
+        loadIAP();
+        
         showLeavingBetaDialog();
     }
     
@@ -302,12 +313,6 @@ public class MainMenuActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
-    }
-    
-    @Override
-    public void onBackPressed() {
-    	super.onBackPressed();
-    	//TODO
     }
     
     public void setActionBar (int numSelections) {
@@ -339,6 +344,68 @@ public class MainMenuActivity extends Activity {
     
     public static boolean isRunning () {
     	return isRunning;
+    }
+    
+    private void loadIAP () {
+    	final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwc8zJDwniUbdnqKiUtkMbrLomID1JaQ0cJLApsX7qbBGU" +
+    			"+78bwUwZ7JlFtBF/ITxstrYXwiiFmB3HKbQaGdTWf2TOb7TK0ysitlcBzHBRYNvD6bsgxtaPwREwT1hql6z+cPrxISyXvRCsvf/T+u9gNGkEkkZeg1SMOL+" +
+    			"ZhOViHCpzQbF6znNu10r771WDdOmv0Ta7W/Qi/icuKf8fKRWhgDh4AzVhmE2z34bGoAZEwkA7AxdedAgdnCe7R6MargdJS9EUh+5PqFsY5QhT115VUbBD2E" +
+    			"W8kcqp6wlj3HuxGdfzevok7fNBQ8qaxSEeWGFCv8dYVLQ4WBnXqIIAA/OSQIDAQAB";
+    	
+    	iabHelper = new IabHelper(this, base64EncodedPublicKey);
+    	
+    	final IabHelper.QueryInventoryFinishedListener queryFinishedListener = new IabHelper.QueryInventoryFinishedListener() {
+			@Override
+			public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+				Log.d("PEN", "Query inventory finished.");
+				
+	            // Have we been disposed of in the meantime? If so, quit.
+	            if (iabHelper == null) {
+	            	Log.d("PEN", "Our IabHelper was disposed of before setup finished.");
+	            	return;
+	            }
+	            
+	            // Did it a fail? If so, quit.
+	            if (result.isFailure()) {
+	                Log.d("PEN", "Failed to query inventory: " + result);
+	                return;
+	            }
+	            
+	            Log.d("PEN", "Query inventory was successful.");
+	            
+	            Purchase proPurchase = inventory.getPurchase(SKU_PRO);
+	            hasProLisence = (proPurchase != null);
+	            Log.d("PEN", "User is " + (hasProLisence ? "PRO" : "NOT PRO"));
+			}
+		};
+		
+    	final IabHelper.OnIabSetupFinishedListener setupFinishedListener = new IabHelper.OnIabSetupFinishedListener() {
+			@Override
+			public void onIabSetupFinished(IabResult result) {
+				Log.d("PEN", "IabHelper setup finished");
+				
+	            if (!result.isSuccess()) {
+	                Log.d("PEN", "There was a problem setting up in-app billing: " + result);
+	                return;
+	            }
+	
+	            // Have we been disposed of in the meantime? If so, quit.
+	            if (iabHelper == null) {
+	            	Log.d("PEN", "Our IabHelper was disposed of before setup finished.");
+	            	return;
+	            }
+	
+	            // IAB is fully set up. Now, let's get an inventory of stuff we own.
+	            Log.d("PEN", "Setup successful. Querying inventory.");
+	            iabHelper.queryInventoryAsync(queryFinishedListener);
+			}
+    	};
+    	
+    	iabHelper.startSetup(setupFinishedListener);
+    }
+    
+    public boolean getIsPro () {
+    	return this.hasProLisence;
     }
     
     private void showLeavingBetaDialog () {
