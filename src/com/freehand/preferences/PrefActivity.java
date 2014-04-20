add253fc463f17e2f9f98994b9969f159e95a0f2
Cal Hinshaw <calhinshaw@gmail.com>
2014-04-20 15:10:53 -0700

    now passing intent result to PrefFragment

--- src/com/freehand/preferences/PrefActivity.java
+++ src/com/freehand/preferences/PrefActivity.java
@@ -2,6 +2,7 @@ package com.freehand.preferences;
 
 import android.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PrefActivity extends Activity {
@@ -9,6 +10,13 @@ public class PrefActivity extends Activity {
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setTitle("Settings");
-		getFragmentManager().beginTransaction().replace(R.id.content, new PrefFragment()).commit();
+		getFragmentManager().beginTransaction().replace(R.id.content, new PrefFragment(), "frag").commit();
 	}
+	
+	
+	@Override
+    protected void onActivityResult (final int requestCode, final int resultCode, final Intent data) {
+    	getFragmentManager().findFragmentByTag("frag").onActivityResult(requestCode, resultCode, data);
+    	super.onActivityResult(requestCode, resultCode, data);
+    }
 }
